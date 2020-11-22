de773655fe511081f575eaf32fc4f910aec715b6
AmyNeko <nichaporn.kalantanon@gmail.com>
2020-11-22 16:57:25 +0700

    switch to mqtt

--- app/src/main/java/com/virachlabo/temiapp/MainActivity.java
+++ app/src/main/java/com/virachlabo/temiapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.virachlabo.temiapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -7,34 +9,53 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 import com.robotemi.sdk.Robot;
 import com.robotemi.sdk.TtsRequest;
+import com.robotemi.sdk.UserInfo;
 import com.robotemi.sdk.exception.OnSdkExceptionListener;
 import com.robotemi.sdk.exception.SdkException;
 import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener;
-import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
-import com.robotemi.sdk.listeners.OnRobotLiftedListener;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
-import com.robotemi.sdk.permission.OnRequestPermissionResultListener;
-import com.robotemi.sdk.permission.Permission;
+import com.robotemi.sdk.listeners.OnTelepresenceEventChangedListener;
+import com.robotemi.sdk.model.CallEventModel;
+import com.robotemi.sdk.navigation.listener.OnCurrentPositionChangedListener;
+import com.robotemi.sdk.navigation.model.Position;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class MainActivity extends AppCompatActivity implements
+public class MainActivity<RobotActionExecutorService> extends AppCompatActivity implements
         OnRobotReadyListener,
-        OnRobotLiftedListener,
         Robot.TtsListener,
-        OnGoToLocationStatusChangedListener,
         OnSdkExceptionListener,
-        OnRequestPermissionResultListener {
+        OnCurrentPositionChangedListener,
+        OnTelepresenceEventChangedListener,
+        OnGoToLocationStatusChangedListener {
 
     //Permission
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -53,25 +74,28 @@ public class MainActivity extends AppCompatActivity implements
     };
 
     //Object and Variable
-    private  Robot robot;
-    private List<String> location;
-    private FirebaseDatabase firebaseDatabase;
+    private Robot robot;
+
+
+    //TEST new movemvent
+    private Position currentPosition;
+    private Position lastPosition;
+    private String gotoStatus;
+
+    //MQTT Instance
+    MqttWrapper mqttWrapper;
+
+    //ActrionFlag
+    Boolean isRobotActionComplete = true;
 
     //Verify permission function
     public static void verifyStroagePermission(Activity activity) {
         int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if(permission != PackageManager.PERMISSION_GRANTED) {
+        if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
         }
     }
-    //Robot Ability Fucntion
-    public void TEST() {
-        DatabaseReference test = firebaseDatabase.getReference();
 
-        test.setValue("I'm READy");
-
-
-    }
     public void RobotSpeak(String s, boolean isOverlayed) {
         robot.speak(TtsRequest.create(s, isOverlayed));
     }
@@ -82,19 +106,14 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         verifyStroagePermission(this);
-
-        robot = Robot.getInstance();
-        robot.addOnRequestPermissionResultListener(this);
+        robot = Robot.getInstance(); // get an instance of the robot in order to begin using its features.
         robot.addOnSdkExceptionListener(this);
-
-        FirebaseApp.initializeApp(this);
+        robot.addOnCurrentPositionChangedListener(this);
 
     }
 
     @Override
     protected void onDestroy() {
-        robot.removeOnRequestPermissionResultListener(this);
-        robot.removeOnSdkExceptionListener(this);
         super.onDestroy();
     }
 
@@ -102,38 +121,37 @@ public class MainActivity extends AppCompatActivity implements
     protected void onStart() {
         super.onStart();
         robot.addOnRobotReadyListener(this);
-        robot.addOnGoToLocationStatusChangedListener(this);
         robot.addTtsListener(this);
-        robot.addOnRobotLiftedListener(this);
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
+
     }
 
-    //Robot callback -------------------------------------------------------
+    // ------------------------------------------------------ Robot SDK Callvack -------------------------------------------------------------
     @Override
     public void onRobotReady(boolean isReady) {
-        if(isReady) {
-            //Iniitializa robot
+        if (isReady) {
             try {
                 final ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
-                robot.onStart(activityInfo);
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
+//                Robot.getInstance().onStart(); //method may change the visibility of top bar.
 
-            TEST();
+                robot.onStart(activityInfo);
+                //Start MQTT Server
+                speak("Fuck you Temi");
+                StartMqtt();
+
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
 
         }
     }
 
     @Override
     public void onTtsStatusChanged(@NotNull TtsRequest ttsRequest) {
-
-        if(ttsRequest.getStatus() == TtsRequest.Status.COMPLETED) {
-            Log.i("TtsStatus", "" + ttsRequest.getStatus().toString());
+        if (ttsRequest.getStatus() == TtsRequest.Status.COMPLETED) {
+            mqttWrapper.publish("actionState/", "Done");
+            isRobotActionComplete = true;
         }
-
-
     }
 
     @Override
@@ -142,18 +160,142 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
+    public void onTelepresenceEventChanged(@NotNull CallEventModel callEventModel) {
+        printLog("onTelepresenceEvent", callEventModel.toString());
+        if (callEventModel.getType() == CallEventModel.TYPE_INCOMING) {
+            Toast.makeText(this, "Incoming call", Toast.LENGTH_LONG).show();
+        }
+        else if  (callEventModel.getType() == CallEventModel.TYPE_OUTGOING) {
+            Toast.makeText(this, "Outgoing call", Toast.LENGTH_LONG).show();
+        }
+        else if (callEventModel.getType() == CallEventModel.STATE_ENDED) {
+            mqttWrapper.publish("actionState/", "Done");
+            isRobotActionComplete = true;
+        }
+    }
+
+
+    @Override
     public void onGoToLocationStatusChanged(@NotNull String location, @NotNull String status, int descriptionId, @NotNull String description) {
-
+        gotoStatus = status;
+        switch (status) {
+            case OnGoToLocationStatusChangedListener.START:
+                printLog("Start");
+                break;
+            case OnGoToLocationStatusChangedListener.CALCULATING:
+                printLog("Calculating route");
+                break;
+            case OnGoToLocationStatusChangedListener.COMPLETE:
+                printLog("Arrived");
+                isRobotActionComplete = true;
+                break;
+        }
     }
 
     @Override
-    public void onRobotLifted(boolean isLifted, @NotNull String reason) {
+    public void onCurrentPositionChanged(@NotNull Position position) {
+        Log.i("POSITION", "" + position.toString());
+        currentPosition = position;
 
     }
 
+    // ------------------------------------------------------ Robot Function  -------------------------------------------------------------
+    public void speak(String tts) {
+        TtsRequest ttsRequest = TtsRequest.create(tts, true);
+        robot.speak(ttsRequest);
+    }
 
-    @Override
-    public void onRequestPermissionResult(@NotNull Permission permission, int grantResult, int requestCode) {
+    public void GetAllUser() {
+        List<UserInfo> userInfos = robot.getAllContact();
+        for (UserInfo userInfo : userInfos) {
+            printLog("Username", userInfo.getName());
+            printLog("UserID", userInfo.getUserId());
+            printLog("UserRole", String.valueOf(userInfo.getRole()));
+        }
 
+    }
+
+    public void goToPosition(float xp, float yp, float yawnp) {
+        try {
+            float x = xp;
+            float y = yp;
+            float yaw = yawnp;
+            robot.goToPosition(new Position(x, y, yaw, 0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goTo(String des) {
+        for (String location : robot.getLocations()) {
+            if (location.equals(des)) {
+                robot.goTo(des);
+            }
+        }
+    }
+
+    void printLog(String msg) {
+        Log.i("RobotAction", msg);
+    }
+
+    void printLog(String tag, String msg) {
+        Log.i(tag, msg);
+    }
+
+    //----------------------------- MQTT --------------------------------------
+    private void StartMqtt() {
+        printLog("Inside MQTT START");
+        mqttWrapper = new MqttWrapper(getApplicationContext());
+        mqttWrapper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                printLog("Connect to " + serverURI.toString());
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                mqttWrapper.reconnect();
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                printLog("From topic: " + topic.toString());
+                printLog("Arrived message: " + message.toString());
+                try {
+                    JSONObject jsonObject = new JSONObject(message.toString());
+                    if(isRobotActionComplete == true) {
+                        isRobotActionComplete = false;
+                        actionDecoder(jsonObject);
+                    }
+                }catch (JSONException e) {
+                    Log.d("Error", e.toString());
+                }
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+    }
+
+    /*---------------- ACTION DECODEER -----------------*/
+    public void actionDecoder(JSONObject actionInfo) {
+        try {
+            switch (actionInfo.get("action").toString()) {
+                case "SPEAK":
+                    speak(actionInfo.get("content").toString());
+                    break;
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+//Async Task
+
+
+
---
+++ app/src/main/java/com/virachlabo/temiapp/MqttWrapper.java
@@ -0,0 +1,118 @@
+package com.virachlabo.temiapp;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MqttWrapper {
+    public MqttAndroidClient mqttAndroidClient;
+
+    final String serverUri = "tcp://babyai.org:1883";
+    final String clientID = "VirachLabo-Temi";
+    final String username = "wifimod";
+    final String password = "PeEFc9Aq";
+
+    List<String> subscriptionTopic = new ArrayList<String>();
+
+    public MqttWrapper(Context context) {
+        mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientID);
+        SubscribeTopic("temi-cmd/");
+        connect();
+
+    }
+
+    //MQTT Callback
+     public void setCallback(MqttCallbackExtended callback) {
+        mqttAndroidClient.setCallback(callback);
+     }
+
+     private void connect() {
+         MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+         mqttConnectOptions.setCleanSession(true);
+         mqttConnectOptions.setUserName(username);
+         mqttConnectOptions.setPassword(password.toCharArray());
+         try {
+             mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                 @Override
+                 public void onSuccess(IMqttToken asyncActionToken) {
+                     DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                     disconnectedBufferOptions.setBufferEnabled(true);
+                     disconnectedBufferOptions.setBufferSize(100);
+                     disconnectedBufferOptions.setPersistBuffer(false);
+                     disconnectedBufferOptions.setDeleteOldestMessages(false);
+
+                     ListIterator itr = subscriptionTopic.listIterator();
+                     while(itr.hasNext()) {
+                         subscribeToTopic((String) itr.next());
+                     }
+                 }
+
+                 @Override
+                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                     Log.w("MQTT", "Fail to connect to " + serverUri + " with exception: " + exception.toString());
+
+                 }
+             });
+         } catch (MqttException e) {
+             e.printStackTrace();
+         }
+
+     }
+     private void subscribeToTopic(String subTopic) {
+        try {
+            //test
+
+            mqttAndroidClient.subscribe(subTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.w("MQTT", "Successfully subscribe to topic " + subTopic);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w("MQTT", "Successfully fail");
+                }
+            });
+
+        }catch (MqttException ex) {
+//            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+     }
+     public void reconnect() {
+        connect();
+     }
+     public void SubscribeTopic(String subTopic){
+         try {
+             subscriptionTopic.add(subTopic);
+         }
+         catch (Exception e) {
+             e.printStackTrace();
+         }
+     }
+
+     public void publish(String topic, String msg) {
+
+        MqttMessage mqttMessage = new MqttMessage(msg.getBytes());
+        try {
+            mqttAndroidClient.publish(topic, mqttMessage);
+        }
+        catch (MqttException e) {
+            e.printStackTrace();
+        }
+     }
+}
